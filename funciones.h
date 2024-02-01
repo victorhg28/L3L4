6fd4cccb0a4fa61b1d02e6ba62e78360ef5af1e5
Victor Paul Morante Vargas <C26681@TIM.COM.PE>
2024-02-01 11:11:52 -0500

    empezando de cero, ahora puedo extraer valores de interfaz exitosamente

--- funciones.h
+++ funciones.h
@@ -7,52 +7,10 @@ using namespace std;
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 // declaración de funciones 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
-string func_sacar_mascara(string str_ip_con_mascara);
-string func_sacar_ip(string str_ip_con_mascara);
-string func_formatoNumeracion(int entrada);
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 // definicion de funciones
 /////////////////////////////////////////////////////////////////////////////////////////////////////
-string func_sacar_mascara(string str_ip_con_mascara){
-	
-	size_t found = str_ip_con_mascara.find("/");
-	if (found != string::npos){
-		return str_ip_con_mascara.substr(found+1);
-	}
-    return "error en funcion sacar mascara";
-}
 
-string func_sacar_ip(string str_ip_con_mascara){
-	
-	size_t found = str_ip_con_mascara.find("/");
-	if (found != string::npos){
-		return str_ip_con_mascara.substr(0,found);
-	}
-    return "error en funcion sacar ip";
-}
-
-string func_formatoNumeracion(int entrada){
-	
-	// formato 000
-	if (entrada<10){
-		return "00"+to_string(entrada);
-		//.size()
-	}
-	else if (entrada<100){
-		return "0"+to_string(entrada);
-	}
-	else{
-		return to_string(entrada);
-	}
-}
-
-
-//probando sub strings
-//	string hola = "hola/200";
-//	size_t found = hola.find("/");
-//	if (found != string::npos){
-//		cout<<hola.substr(found+1);
-//	}
 #endif
--- main.cpp
+++ main.cpp
@@ -1,38 +1,87 @@
-#include "L3L4.h"
-# include <string>
+# include "L3L4.h"
+# include "objetos.h"
+#include <typeinfo>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#ifndef header_funciones
+	#define header_funciones
+	#include "funciones.h"
+#endif
+
+#ifndef header_objetos
+	#define header_objetos
+	#include "objetos.h"
+#endif
 
 using namespace std;
 
 comandos_L3L4::comandos_L3L4()
 {
 	CtrlLayout(*this, "L3L4");
-	
-	/*
-	date1 ^= date2 ^= [=] {
-		result = IsNull(date1) || IsNull(date2) ? "" :
-		         Format("There is %d day(s) between %` and %`", abs(Date(~date1) - Date(~date2)), ~date1, ~date2);
-	};
-	*/
-	//miTexto.SetData("asdasdass");
 	btn_1 <<= THISBACK(Click);
 }
 
 void comandos_L3L4::Click(){
-	//funcion();
-	//string aux;
+	
+	
+	//f_titok_r123123_001
+	
+	//struct
+	//nombreFiltro nFiltro;
+	
+	String Str_acum;
+	
+	//probando num requeriString(gui_numReqmiento
+	//gui_resultado.SetText(FormatInteger(gui_numReq));
+	
+	//probando nombre (EditString)
+	//gui_resultado.SetText(Format(gui_nomServ));
+	//gui_resultado.SetText((String)gui_nomServ.GetData());
+	
+	Str_acum = "f_"+(String)gui_nomServ.GetData()+"_r"+FormatInteger(gui_numReq)+"__01";
+	gui_resultado.SetText(Str_acum);
+	
+	//(gui_nomServ));
+	
+	
+	
+	
+	//f_titok_r123123_001
+	//nFiltro.pre = "f_";
+	//nFiltro.nombre = (String)gui_nomServ.GetData();
+	//nFiltro.req = (String)gui_numReq.GetData();
+	//nombre.num_auto = 123123;
+	//nFiltro.resultado = nFiltro.pre + nFiltro.nombre + "_r" + nFiltro.req +"_porAhoraSinNumeracion";
+	
+	//String str_prompt = nFiltro.resultado;
+	//nFiltro.resultado.Cat(nFiltro.pre);
+	
+	//std::string adasdasd="asd";
+	
+	//PromptOK((String)nFiltro.resultado);
+	//PromptOK();
+	//gui_resultado.SetText("nop");
+	
+	//String abc = ""+nFiltro.resultado;
+	//gui_resultado.SetText(abc);
+	
+	//PromptOK( (String)gui_nomServ.GetData()+"asdasd" );
+	
+	
+	
+	//gui_nomServ.GetData();
+	
+	//typeid(gui_nomServ.GetData()).name();		//N3Upp5ValueE
+	//String aka="_"; //N3Upp6StringE
+	
+	//PromptOK(typeid(gui_nomServ.GetData()).name());
+	//PromptOK( (String)gui_nomServ.GetData()+"asdasd" );
+	//PromptOK(   typeid(aka).name()   );
 	
 	/*
-	string parte1 = "ADD FILTER:FILTERNAME=\"";
-	string parte3 = "\",L34PROTTYPE=STRING,L34PROTOCOL=ANY,SVRIPMODE=IP,SVRIP=\"";// más IP
-	string parte4 = "\",SVRIPMASKTYPE=LENGTHTYPE,SVRIPMASKLEN=";
-	string mascara_aux= func_sacar_mascara(s_ipEnBruto);
-	//string parte5 = ",MSSTARTPORT=0,MSENDPORT=65535,SVRSTARTPORT=0,SVRENDPORT=65535;";
-	string parte5 = ",MSSTARTPORT=0,MSENDPORT=65535,SVRSTARTPORT=";
-	string parte6 = ",SVRENDPORT=";
-	cout<<parte1+s_filtername+parte3+func_sacar_ip(s_ipEnBruto)+parte4+mascara_aux+parte5+s_svrstartport+parte6+s_svrendport+";";
-	
-	*/
-	
 	String parte1 = "ADD FILTER:FILTERNAME=\"";
 	String parte3 = "\",L34PROTTYPE=STRING,L34PROTOCOL=ANY,SVRIPMODE=IP,SVRIP=\"";// más IP
 	String parte4 = "\",SVRIPMASKTYPE=LENGTHTYPE,SVRIPMASKLEN=";
@@ -46,80 +95,18 @@ void comandos_L3L4::Click(){
 	int num_aux=gui_puerto.GetData();;
 	string str_aux=to_string(num_aux);
 	
-	
 	String aux;
-	//aux=parte1+s_filtername+parte3+func_sacar_ip(s_ipEnBruto)+parte4+mascara_aux+parte5+s_svrstartport+parte6+s_svrendport+";";	
 	aux = parte1+"nombre_generico"+parte3+"ip_generica"+parte4+mascara_aux+parte5+str_aux+parte6+str_aux+";";
 
 	PromptOK(aux);
 	
-	//PromptOK((String)gui_puerto.GetData());
 	
-	/*
-	int num;
-	string abc;
-	num=gui_puerto.GetData();
-	abc=to_string(num);
-	PromptOK(   (String)abc   );
+	
+	//f_titok_r123123_001
 	*/
-	
-	/* ESTEEEE!
-	int num_aux=gui_puerto.GetData();;
-	string str_aux=to_string(num_aux);
-	PromptOK(   (String)str_aux   );
-	*/
-	
-	//PromptOK((String)std::to_string(gui_puerto.GetData()));
-	
-	
-	//PromptOK((String)std::to_string(gui_puerto.GetData()));
-	//string rare;
-	//rare=static_cast<String>(gui_puerto.GetData());
-	//PromptOK(  static_cast<String>(gui_puerto.GetData())   );
-	
-	
-	
-	
-	
-	
-	
-	//PromptOK(to_string(valor));
-	
-	
-	/*
-	String cadena;
-	cadena=(String)gui_puerto.GetData();
-	PromptOK(cadena);
-	*/
-	
-	
-	
-	
-	
-	
-	
-	//PromptOK((String)gui_puerto.GetData());
-	
-	//PromptOK(gui_puerto.GetInc());
-	
-	//printf("Your age is %d", 13);
-	
-	
-	//PromptOK(std::to_string(num));
-
-	//aux="adad";
-	//PromptOK((String)gui_nomReq.GetData());
-	//PromptOK(aux);
 }
 
-void comandos_L3L4::funcion()
-{
-	//PromptOK("generic text");
-	PromptOK((String)gui_nomReq.GetData());
-	//gui_numReq
-}
 
-GUI_APP_MAIN
-{
+GUI_APP_MAIN{
 	comandos_L3L4().Run();
 }

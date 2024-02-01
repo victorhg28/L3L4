554f033a8d27cd4b32efe8cf3b1b3e3de5bd9590
Victor Paul Morante Vargas <C26681@TIM.COM.PE>
2024-02-01 12:12:53 -0500

    generacion de primer comando con nombre de filtro exitoso

--- L3L4.h
+++ L3L4.h
@@ -15,6 +15,7 @@ public:
 	void Click();
 	comandos_L3L4();
 	//String Str_nombreFiltro();
+	String comandoCreacionFiltro(String nombre_f);
 };
 
 struct nombreFiltro{
@@ -24,38 +25,5 @@ struct nombreFiltro{
 	String num_auto;
 	String resultado;
 };
-
-
-/*
-class comando{
-	public:
-	//parametros
-	String parte1 = "ADD FILTER:FILTERNAME=\"";
-	String parte3 = "\",L34PROTTYPE=STRING,L34PROTOCOL=ANY,SVRIPMODE=IP,SVRIP=\"";// más IP
-	String parte4 = "\",SVRIPMASKTYPE=LENGTHTYPE,SVRIPMASKLEN=";
-	//String mascara_aux= func_sacar_mascara(s_ipEnBruto);
-	String mascara_aux="noFuncional_por_ahora";
-	//string parte5 = ",MSSTARTPORT=0,MSENDPORT=65535,SVRSTARTPORT=0,SVRENDPORT=65535;";
-	String parte5 = ",MSSTARTPORT=0,MSENDPORT=65535,SVRSTARTPORT=";
-	String parte6 = ",SVRENDPORT=";
-	
-	//PARA PUERTO
-	String num_aux=0;
-	
-	//f_titok_r123123_001
-	
-	// constructor
-	comando (String x, String y) {
-		num = x;
-		texto = y;
-	}
-	
-	
-	//String aux;
-	aux = parte1+"nombre_generico"+parte3+"ip_generica"+parte4+mascara_aux+parte5+str_aux+parte6+str_aux+";";
-
-	PromptOK(aux);
-};
-*/
 
 #endif
--- main.cpp
+++ main.cpp
@@ -24,8 +24,11 @@ comandos_L3L4::comandos_L3L4()
 	btn_1 <<= THISBACK(Click);
 }
 
+
+
 void comandos_L3L4::Click(){
 	
+	//Creacion nombre de filtro
 	
 	//f_titok_r123123_001
 	
@@ -42,11 +45,77 @@ void comandos_L3L4::Click(){
 	nFiltro.resultado += nFiltro.req;
 	nFiltro.resultado += "_"+nFiltro.num_auto;
 	
-	gui_resultado.SetText(nFiltro.resultado);
+	//gui_resultado.SetText(nFiltro.resultado);
+	//gui_resultado.SetText(comandoCreacionFiltro(nFiltro.resultado));
+	
+	// creacion de archivo para guardar resultado
+	// Create and open a text file
+	ofstream archivo_resultado("filename.txt");
+
+	// Write to the file
+	//archivo_resultado << "Files can be tricky, but it is fun enough!";
+	archivo_resultado << comandoCreacionFiltro(nFiltro.resultado).ToStd();
+
+	// Close the file
+	archivo_resultado.close();
+	
+	
+	
+	//String
+	
+	/*
+	//lectura de archivo IPs
+	
+	//abriendo archivo
+	ifstream ifstream_archivo("ips.txt");
+	
+	//variable para leer lineas
+	string str_aux_text;
+	
+	//generando comando
+	while (getline (ifstream_archivo, str_aux_text)) {
+		//i_auxNum++;
+  		
+  		comando obj_comando(nFiltro.resultado,str_aux_text,dat1.str_puerto,dat1.str_puerto);
+  		obj_comando.imprimir();
+  		
+  		str_auxNombreFiltro=obj_nombreFiltro.m_imprimir();
+  		
+		//cout<<func_generandoBindeado(i_auxNum,obj_nombreFiltro.m_imprimir(), dat1.str_flowFilter);
+		//cout<<"\n";
+	}
+
+	// Cerrando archivo
+	ifstream_archivo.close();
+	*/
+	
 	
 }
 
 
 GUI_APP_MAIN{
-	comandos_L3L4().Run();
+	comandos_L3L4 programa;
+	
+	programa.Sizeable();
+	programa.Run();
+}
+
+String comandos_L3L4::comandoCreacionFiltro(String nombre_f){
+	//String s_filtername = nombre_f;
+	//String s_ipEnBruto;
+	//String s_svrstartport;
+	//String s_svrendport;
+	
+	String aux = "ADD FILTER:FILTERNAME=\"";
+	aux += nombre_f + "\",L34PROTTYPE=STRING,L34PROTOCOL=ANY,SVRIPMODE=IP,SVRIP=\"";
+	aux += "ip_generica";
+	aux += "\",SVRIPMASKTYPE=LENGTHTYPE,SVRIPMASKLEN=";
+	aux += "mascara_generica";
+	aux += ",MSSTARTPORT=0,MSENDPORT=65535,SVRSTARTPORT=";
+	aux += "*puerto*";
+	aux += ",SVRENDPORT=";
+	aux += "*puerto*";
+	aux += ";";
+	
+	return aux;
 }
